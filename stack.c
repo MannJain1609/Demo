bae1269444be3afc2098937fd3ad57b0200021ff
MannJain1609 <75989468+MannJain1609@users.noreply.github.com>
2020-12-17 12:20:58 +0530

    timeline stack

--- stack.c
+++ stack.c
@@ -1,52 +1,6 @@
-#include "stack.h"
 #include<stdio.h>
 
-void push(struct Stack *s,char val)
+void main()
 {
-	if (isFull(s))
-	{
-		printf("Stack Overflow\n");
-		return;
-	}
-	s->arr[++(s->top_index)]=val;
-}
-
-char pop(struct Stack *s)
-{
-	if (isEmpty(s))
-	{
-		printf("Stack Underflow\n");
-		return '0';
-	}
-	return s->arr[(s->top_index)--];
-}
-
-char top(struct Stack *s)
-{
-	if (isEmpty(s))
-	{
-		printf("Stack Underflow\n");
-		return '0';
-	}
-	return s->arr[s->top_index];
-}
-
-int isEmpty(struct Stack *s)
-{
-	if (s->top_index==-1)
-		return 1;
-	return 0;
-}
-
-int isFull(struct Stack *s)
-{
-	if (s->top_index==s->capacity-1)
-		return 1;
-	return 0;
-}
-
-void initialise(struct Stack *s)
-{
-	s->capacity = 100;
-	s->top_index = -1;
+	printf("Hii");
 }
